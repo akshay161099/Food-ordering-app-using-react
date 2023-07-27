7abfe455ef878a9516407e186ae9b36df63eac0f
Akshay Kumar <akshaykumar161099@gmail.com>
2023-07-27 20:32:44 +0530

    commit

---
+++ App.js
@@ -0,0 +1,3 @@
+const heading = React.createElement("h1",{id:"heading"},[React.createElement("h1",{},"hello"),React.createElement("h2",{},"World")]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
