04704d552384698f1bf3e5256c9ff3a9b2b3263b
Akshay Kumar <akshaykumar161099@gmail.com>
2023-08-07 19:58:19 +0530

    third commit

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -6,12 +6,11 @@ const RestaurantCard = (props) => {
     return (
         <div className="res-card">
             <img className="res-logo" src={RES_LOGO} alt="KFC"></img>
-            <h4>{name}hello</h4>
-            <h4>{cuisines}</h4>
+            <h4>{name}</h4>
+            <h4>{cuisines.join(", ")}</h4>
             <h4>{deliveryTime}</h4>
             <h4>{rating}</h4>
         </div>
-
     );
 }
 export default RestaurantCard;
